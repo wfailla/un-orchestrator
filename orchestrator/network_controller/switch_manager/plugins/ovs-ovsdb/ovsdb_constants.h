c68019efa0aba8c52ac50f89ed73f8d28f76ab31
Patrick Tomassone <ptoma.90@hotmail.it>
2015-10-23 18:30:50 +0200

    Code refactor in OVS code

--- orchestrator/network_controller/switch_manager/plugins/ovs-ovsdb/ovsdb_constants.h
+++ orchestrator/network_controller/switch_manager/plugins/ovs-ovsdb/ovsdb_constants.h
@@ -6,13 +6,8 @@
 *	(The script path is followed by some %s, exploited by the code to create the proper command line
 */
 
-#define PATH_SCRIPT_VIRTUAL_LINK 	"./network_controller/switch_manager/plugins/ovs/scripts/VirtualLink.sh %s %s %s %s"
-#define PATH_SCRIPT_ID_PORT			"./network_controller/switch_manager/plugins/ovs/scripts/IdPort.sh %s %s"
-#define PATH_SCRIPT_OF_VERSION		"./network_controller/switch_manager/plugins/ovs/scripts/OFVersion.sh %s %s"
-
-#define TMP_XML_FILE				"file.xml"
-
 #define SOCKET_IP					"127.0.0.1"
 #define SOCKET_PORT					"6632"
+#define NUM_UUID					3
 
 #endif //OVS_CONSTANTS_H_
