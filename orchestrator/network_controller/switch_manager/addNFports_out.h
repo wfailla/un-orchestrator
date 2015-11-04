69633b8d0fc6e166fddce46054d31f51f4dad9e1
ivanocerrato <8002onavi@gmail.com>
2015-11-04 09:00:34 +0100

    Improved documentation of parameters returned by the network controller.

--- orchestrator/network_controller/switch_manager/addNFports_out.h
+++ orchestrator/network_controller/switch_manager/addNFports_out.h
@@ -28,11 +28,18 @@ private:
 
 	/**
 	*	@brief: map of ports name, identifier within the lsi
+	*		The name of the network function ports is that selected by the node resource manager based on the graph, and will
+	*		be later used by the node resource manager to properly create the graph.
 	*/
 	map<string, unsigned int> ports;
 	
 	/**
 	*	@brief: list of ports on the vSwitch that are associated with the network function
+	*		The name of the port created on the switch may differ from the name used by the node resource manager to refer to
+	*		such a port. Since the name of the ports created on the switch must be used by the compute controller and conneted 
+	*		to the network function, these names must be returned to the node resource manager.
+	*		Note that the name of the ports defined by the node resource manager and the name of the ports connected to the 
+	*		vSwitch can be the same.
 	*/
 	list<string> ports_name_on_switch;
 	
--- orchestrator/network_controller/switch_manager/createLSIout.h
+++ orchestrator/network_controller/switch_manager/createLSIout.h
@@ -30,11 +30,18 @@ private:
 		
 	/**
 	*	@brief: map of network functions name, map of network fuction ports name, network function ports identifier on the lsi
+	*		The name of the network function ports is that selected by the node resource manager based on the graph, and will
+	*		be later used by the node resource manager to properly create the graph.
 	*/
 	map<string,map<string, unsigned int> >  network_functions_ports;
 	
 	/**
-	*	@brief: map of network functions name, list of ports on the vSwitch that are associated with such a network function
+	*	@brief: map of network functions name, list of ports on the vSwitch that are associated with such a network function.
+	*		The name of the port created on the switch may differ from the name used by the node resource manager to refer to
+	*		such a port. Since the name of the ports created on the switch must be used by the compute controller and conneted 
+	*		to the network function, these names must be returned to the node resource manager.
+	*		Note that the name of the ports defined by the node resource manager and the name of the ports connected to the 
+	*		vSwitch can be the same.
 	*/
 	map<string,list<string> > nf_ports_name_on_switch;
 	
