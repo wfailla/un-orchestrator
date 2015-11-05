90a0de6ed2aa100ff4489e96907a64019fa24754
snuccio <sergio.nuccio@hotmail.it>
2015-11-05 13:14:39 +0100

    Insert linux bridge in example of native NF

--- NFs/native/example/nativeNF_example.sh
+++ NFs/native/example/nativeNF_example.sh
@@ -26,14 +26,24 @@ sysctl -w net.ipv4.ip_forward=1
 #debug
 #set -x
 
+br_name=$1_$2_br
+
+brctl addbr $br_name
+
 current=4
 for (( c=0; c < $3; c++ ))
 do
 
+	brctl addif $br_name ${!current}
+
 	ebtables -A FORWARD -i ${!current} -j mark --set-mark 0x$(echo "obase=16; $1" | bc)cade$(echo "obase=16; $((c+1))" | bc) --mark-target CONTINUE
 
 	current=`expr $current + 1`
-done 
+done                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+ifconfig $br_name up
+
+brctl show $br_name
 
 #write file that specifies actions to do in order to stop the NF and clean the system
 stop_file="$1_$2_stop"
@@ -46,10 +56,14 @@ echo "" > $stop_file
 current=4
 for (( c=0; c < $3; c++))
 do
+	
 	echo ebtables -D FORWARD -i ${!current} -j mark --set-mark 0x$(echo "obase=16; $1" | bc)cade$(echo "obase=16; $((c+1))" | bc) --mark-target CONTINUE >> $stop_file
 	current=`expr $current + 1`
 done
 
+echo ifconfig $br_name down >> $stop_file
+
+echo brctl delbr $br_name >> $stop_file
 
 echo "[nativeNF_example] script executed"
 
