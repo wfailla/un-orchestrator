b26a36691fdffa898145afb90cbffe37a27cde9f
Ivano <8002onavi@gmail.com>
2016-01-18 15:41:58 +0100

    Better error handling in ovs-ovsdb plugin.

--- orchestrator/network_controller/switch_manager/plugins/ovs-ovsdb/commands.cc
+++ orchestrator/network_controller/switch_manager/plugins/ovs-ovsdb/commands.cc
@@ -647,12 +647,23 @@ void commands::add_port(string p, uint64_t dnumber, bool is_nf_port, int s, Port
 		switch (port_type) {
 		case IVSHMEM_PORT:
 		case DPDKR_PORT:
-			assert(0 && "Currently supported by the OvS-DPDK plugin");
 			row["type"] = "dpdkr";
+			
+			
+			//XXX the next rows have to be removed when this plugin with support OvS-DPDK
+			logger(ORCH_WARNING, OVSDB_MODULE_NAME, __FILE__, __LINE__, "Currently supported by the OvS-DPDK plugin");
+			assert(0 && "Currently supported by the OvS-DPDK plugin");
+			throw OVSDBManagerException();
+			
 			break;
 		case USVHOST_PORT:
-			assert(0 && "Currently supported by the OvS-DPDK plugin");
 			row["type"] = "dpdkvhostuser";
+			
+			//XXX the next rows have to be removed when this plugin with support OvS-DPDK
+			logger(ORCH_WARNING, OVSDB_MODULE_NAME, __FILE__, __LINE__, "Currently supported by the OvS-DPDK plugin");
+			assert(0 && "Currently supported by the OvS-DPDK plugin");
+			throw OVSDBManagerException();
+			
 			break;
 		case VETH_PORT:
 		{		
