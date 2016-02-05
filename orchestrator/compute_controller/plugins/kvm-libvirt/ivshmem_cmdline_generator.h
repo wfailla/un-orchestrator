590397b4a8e70fa88bddb732416a8492fe27b3e3
David Verbeiren <david.verbeiren@intel.com>
2016-02-05 17:29:04 +0100

    Introduce port aliases for KVM Libvirt
    This translates to DPDK ring aliases inserted in the per-VM IVSHMEM
    metadata area. It requires a DPDK patch to support ring aliases.

--- orchestrator/compute_controller/plugins/kvm-libvirt/ivshmem_cmdline_generator.h
+++ orchestrator/compute_controller/plugins/kvm-libvirt/ivshmem_cmdline_generator.h
@@ -26,10 +26,11 @@ public:
 	* @brief:	generates the command line for adding a port to a guest
 	*
 	* @param:	port_name	Name of the port related to the command line to be generated
+	* @param:	port_alias	Alias for the port as a name under which it will be available in the VM
 	* @param:	cmdline		Buffer where the command line will be saved
 	* @param:	size		Size of the buffer for the command line
 	*/
-	bool get_port_cmdline(const char * port_name, char * cmdline, int size);
+	bool get_port_cmdline(const char * port_name, const char * port_alias, char * cmdline, int size);
 
 	/*
 	* @brief:	generates the command line for map ovs mempool into a guest
@@ -39,15 +40,15 @@ public:
 	*/
 	bool get_mempool_cmdline(char * cmdline, int size);
 
-    /*
-    * @brief:   generates the command line for mapping mempool(s) and ports into a guest
-    *
-    * @param:   cmdline     Buffer where the command line will be saved
-    * @param:   size        Size of the buffer for the command line
-    * @param:   vnf_name
-    * @param:   port_names  List of port names to export
-    */
-    bool get_single_cmdline(char * cmdline, int size, const std::string& vnf_name, std::vector<std::string>& port_names);
+	/*
+	 * @brief:   generates the command line for mapping mempool(s) and ports into a guest
+	 *
+	 * @param:   cmdline     Buffer where the command line will be saved
+	 * @param:   size        Size of the buffer for the command line
+	 * @param:   vnf_name
+	 * @param:   port_names  List of port names to export
+	 */
+	bool get_single_cmdline(char * cmdline, int size, const std::string& vnf_name, std::vector< std::pair<std::string, std::string> >& port_names);
 
 private:
 	bool read_from_pipe(const char *name, char *buf, size_t len);
