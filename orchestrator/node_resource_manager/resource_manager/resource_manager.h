77817856b373bfb708eb081bf77e0525e83be0d0
ivanocerrato <8002onavi@gmail.com>
2016-03-10 09:07:26 +0100

    Added the code of the resource manager.
    This module exports domain information through the double decker network.

---
+++ orchestrator/node_resource_manager/resource_manager/resource_manager.h
@@ -0,0 +1,25 @@
+#ifndef RESOURCE_MANAGER_H_
+#define RESOURCE_MANAGER_H_ 1
+
+#include "../pub_sub/pub_sub.h"
+#include "../../utils/logger.h"
+#include "../../utils/constants.h"
+
+class ResourceManager
+{
+	/**
+	*	@brief: this class exports resources of the Universal Node, by means of the 
+	**		DoubleDecker client
+	*/
+	
+public:
+	/**
+	*	@breif: Publish the domain information written in a specific file
+	*
+	*	@param	descr_file: file containing the descriptio of the domain to
+	*		be exported
+	*/
+	static void publishDescriptionFromFile(char *descr_file);
+};
+
+#endif // RESOURCE_MANAGER_H_
