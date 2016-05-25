9973f6f57dc1c3141f9e145e057ccfbefc62c859
Steven Van Rossem <steven.vanrossem@intec.ugent.be>
2016-05-25 15:16:23 +0200

    update elastic router example files

---
+++ NFs/docker/elastic-router/ctrl_app/start_no_DD.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+#This gives time to the UN to configure the network namespace of the container
+sleep 3
+
+#DD command
+#./ryu_ddclient.py -k /a-keys.json -d tcp://172.17.0.1:7777 ryu a
+
+echo "Control app container started"
+
+
+echo "start ryu"
+cd ryu_app/
+ryu-manager ctrl_app_er_un_zmq_v1.py
+
+# keep container running
+#while true
+#do
+#	sleep 1
+#done
+
